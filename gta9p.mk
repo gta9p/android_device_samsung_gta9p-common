4299a31699c7dec390e6b8a1fc1b29d2a4c54b16
merothh <merothh@gmail.com>
2022-01-02 23:00:09 +0530

    gta9p-common: Copy over public.libraries.txt

--- gta9p.mk
+++ gta9p.mk
@@ -22,6 +22,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
